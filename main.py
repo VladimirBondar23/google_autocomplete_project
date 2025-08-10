b23ee59f237bf8866e99c07380a574ccf4e46d86
Vladimir Bondar <vdecbel@gmail.com>
2025-08-10 13:28:46 +0300

    basic main structure

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import sys
+
+import database as db
+import algo as al
+
 def main():
-    pass
+    dir_name = sys.argv[1]
+    db.store_lines(dir_name)
+    while (True):
+        word = input()
+        al.search(word)
+
 
 
 
