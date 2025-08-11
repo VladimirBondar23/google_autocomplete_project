769e4d2f093efe52c8d120058e91b68a4f794694
Gal Helner <galhelner@gmail.com>
2025-08-11 21:30:46 +0300

    added more integration tests

--- tests/test_auto_completor.py
+++ tests/test_auto_completor.py
@@ -1,5 +1,6 @@
 import pytest
 import auto_completor
+import time
 
 ARCHIVE_DIRECTORY = "../Archive"
 
@@ -48,16 +49,38 @@ def test_input_with_typo(completor):
     input_text_with_typo = "shell scriting"
     completions_with_typo = completor.get_best_k_completions(input_text_with_typo)
     reprs_with_typo = [repr(c) for c in completions_with_typo]
-    assert any(input_text_without_typo in s for s in reprs_with_typo)
+    assert any(input_text_without_typo in s.lower() for s in reprs_with_typo)
 
     # extra character typo
     input_text_with_typo = "shell scripxting"
     completions_with_typo = completor.get_best_k_completions(input_text_with_typo)
     reprs_with_typo = [repr(c) for c in completions_with_typo]
-    assert any(input_text_without_typo in s for s in reprs_with_typo)
+    assert any(input_text_without_typo in s.lower() for s in reprs_with_typo)
 
     # wrong character typo
     input_text_with_typo = "shell screpting"
     completions_with_typo = completor.get_best_k_completions(input_text_with_typo)
     reprs_with_typo = [repr(c) for c in completions_with_typo]
-    assert any(input_text_without_typo in s for s in reprs_with_typo)
+    assert any(input_text_without_typo in s.lower() for s in reprs_with_typo)
+
+
+def test_completion_search_performance(completor):
+    test_inputs = [
+        "my name is",
+        "The Concepts section helps you learn about the parts of the Kubernetes",
+        "shell scripting",
+        "python programming language",
+        "introduction to machine learning"
+    ]
+
+    durations = []
+
+    for text in test_inputs:
+        start = time.perf_counter()
+        completor.get_best_k_completions(text)
+        durations.append(time.perf_counter() - start)
+
+    avg_duration = sum(durations) / len(durations)
+
+    print(f"\nAverage autocomplete time over {len(test_inputs)} inputs: {avg_duration:.3f} seconds")
+    assert avg_duration < 5.5
